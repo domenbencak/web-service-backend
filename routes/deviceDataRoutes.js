c7e500dcf3245b2d7db4b989594b49113260ca2f
Domen Bencak <domen.bencak@student.um.si>
2023-05-14 18:42:17 +0200

    Test 3 adding data to db

--- routes/deviceDataRoutes.js
+++ routes/deviceDataRoutes.js
@@ -12,7 +12,10 @@ router.get('/:id', deviceDataController.show);
 /*
  * POST
  */
-router.post('/', deviceDataController.create);
+router.post('/', function(req, res, next) {
+  console.log('Received POST request to /deviceData'); // Add this line
+  deviceDataController.create(req, res);
+});
 
 /*
  * PUT
