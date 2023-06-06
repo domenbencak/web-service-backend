e8bc5554238e095dcfe5c3876c83bb21a518832a
Domen Bencak <domen.bencak@student.um.si>
2023-06-06 21:00:02 +0200

    Test docker compose 17 (i think this one will work)

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask, request, jsonify
 import base64
-import logging
 import io
 from PIL import Image
-import cv2
 import os
-import time
 from deepface import DeepFace
-import numpy as np
 
 app = Flask(__name__)
 
